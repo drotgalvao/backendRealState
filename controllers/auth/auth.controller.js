9846acc1439809d0d7f3707788c2c564fe020175
Diego Galvão <diegohenriquegalvao@gmail.com>
2023-10-02 15:38:26 -0300

    google signin route added

--- controllers/auth/auth.controller.js
+++ controllers/auth/auth.controller.js
@@ -55,7 +55,6 @@ export const signin = async (req, res, next) => {
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
     const { password: _, ...userWithoutPassword } = user.toObject();
 
-
     res
       .cookie("token", token, {
         httpOnly: true,
@@ -63,7 +62,47 @@ export const signin = async (req, res, next) => {
       })
       .status(200)
       .json(userWithoutPassword);
+  } catch (error) {
+    next(error);
+  }
+};
 
+export const google = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      const { password: _, ...userWithoutPassword } = user.toObject();
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          expires: new Date(Date.now() + 1000 * 60 * 60 * 24),
+        })
+        .status(200)
+        .json(userWithoutPassword);
+    } else {
+      const generatedPassword = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
+      const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
+
+      const newUser = new User({
+        username: req.body.name.split(" ").join("").toLowerCase() + Math.random().toString(36).slice(-4),
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword,
+        avatar: req.body.photo,
+      });
+      await newUser.save();
+      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+      const { password: _, ...userWithoutPassword } = newUser.toObject();
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          expires: new Date(Date.now() + 1000 * 60 * 60 * 24),
+        })
+        .status(200)
+        .json(userWithoutPassword);
+
+    }
   } catch (error) {
     next(error);
   }
