d1621419be7838897833afe0be938d74c3b9f5c1
Diego Galvão <diegohenriquegalvao@gmail.com>
2023-10-03 15:28:22 -0300

    delete user

--- controllers/user/user.controller.js
+++ controllers/user/user.controller.js
@@ -2,36 +2,51 @@ import bcryptjs from "bcryptjs";
 import User from "../../models/user.model.js";
 import { errorHandler } from "../../utils/error.js";
 
-
 export const test = (req, res) => {
-    res.send("Api route is working!");
-}
+  res.send("Api route is working!");
+};
 
 export const updateUser = async (req, res, next) => {
-    if(req.user.id !== req.params.id) {        
-        return next(errorHandler(401, "You are not authorized"));
-    }
-    
-    try {
-        if(req.body.password) {
-            req.body.password = bcryptjs.hashSync(req.body.password, 10);
-        }
+  if (req.user.id !== req.params.id) {
+    return next(errorHandler(401, "You are not authorized"));
+  }
 
-        const updatedUser = await User.findByIdAndUpdate(req.params.id,{
-            $set: {
-                username: req.body.username,
-                name: req.body.name,
-                email: req.body.email,
-                password: req.body.password,
-                avatar: req.body.avatar
-            }
-        }, {new: true});
-
-        const {password, ...withoutPassword} = updatedUser._doc;
-        res.status(200).json(withoutPassword);
-        
-    } catch (error) {
-        next(error);
+  try {
+    if (req.body.password) {
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
     }
-    next();
-}
+
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password,
+          avatar: req.body.avatar,
+        },
+      },
+      { new: true }
+    );
+
+    const { password, ...withoutPassword } = updatedUser._doc;
+    res.status(200).json(withoutPassword);
+  } catch (error) {
+    next(error);
+  }
+  next();
+};
+
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id) {
+    return next(errorHandler(401, "You are not authorized"));
+  }
+
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.clearCookie("token").status(200).json("User has been deleted!");
+  } catch (error) {
+    next(error);
+  }
+};
