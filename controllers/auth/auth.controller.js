20d984342a74d24ed48de29e44cb1f4458a9cd74
Diego Galvão <diegohenriquegalvao@gmail.com>
2023-10-03 17:02:45 -0300

    signout added

--- controllers/auth/auth.controller.js
+++ controllers/auth/auth.controller.js
@@ -107,3 +107,11 @@ export const google = async (req, res, next) => {
     next(error);
   }
 };
+
+export const signout = async (req, res, next) => {
+  try {
+    res.clearCookie("token").status(200).json("User has been signed out!");
+  } catch (error) {
+    next(error);
+  }
+}
