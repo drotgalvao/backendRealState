703ec286f8cb1ac972ea11b02d4abad4f1b82265
Diego <diegohenriquegalvao@gmail.com>
2023-12-03 12:41:03 -0300

    getUser endpoint

--- controllers/user/user.controller.js
+++ controllers/user/user.controller.js
@@ -64,3 +64,18 @@ export const getUserListings = async (req, res, next) => {
     return next(errorHandler(401, "You are not authorized"));
   }
 };
+
+export const getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return next(errorHandler(404, "User not found"));
+    }
+
+    const { password, ...withoutPassword } = user._doc;
+
+    res.status(200).json(withoutPassword);
+  } catch (error) {
+    next(error);
+  }
+};
